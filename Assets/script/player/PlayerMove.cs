3a8fe1a4d81c65cb002c5ed254503a5dff1c3fd4
Kit1994 <klvdbunt@gmail.com>
2013-11-22 08:51:57 +0100

    basic player add + enemy_1 animation

---
+++ Assets/script/player/PlayerMove.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	private bool canJump;
+	public int jumpForce = 800;
+	public int moveHorForce = 20;
+	public int maxSpeed = 10;
+	public float slowDownHorGround = 0.9f;
+	bool grounded;
+
+	void Update (){
+		bool attack = Input.GetButton("Fire1");
+		bool rayHit = false;
+		bool spaceKey = Input.GetKey (KeyCode.Space);
+
+		//attack
+
+		//print(attack);
+		//check if can jump
+		grounded = Physics2D.Linecast(transform.position,(transform.position+Vector3.down*0.6f),
+		           (1 << LayerMask.NameToLayer("Level"))|(1 << LayerMask.NameToLayer("NPC")));
+		if(grounded){
+			if(spaceKey)
+			{
+				rayHit = true; 
+			}
+		}
+		Debug.DrawLine(transform.position,transform.position+(Vector3.down*0.6f));
+		if(rayHit){
+			canJump = true;
+		}
+	}
+	int jumpNum;
+	void FixedUpdate () {
+		//get imput
+		float h = moveHorForce*Input.GetAxis("Horizontal");
+		//maxmove
+		if(Mathf.Abs (rigidbody2D.velocity.x)> maxSpeed){
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+		//slowdown horizontal
+		if(h==0){
+			if(grounded){
+				rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x * slowDownHorGround, rigidbody2D.velocity.y);
+			}
+		}
+		//move horizontal
+		rigidbody2D.AddForce(new Vector2(h,0));
+
+		//jump
+		if(canJump){
+			jumpNum++;
+			Debug.Log("jump"+jumpNum);
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+			canJump = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		//Debug.Log( col.);
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ Assets/script/spawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+
+	public GameObject spawnOject;
+	public int spawnSec = 150;
+	private int timer = 0;
+	void FixedUpdate () {
+		timer++;
+		if(timer%spawnSec==0){
+		for (int i = 0; i<1 ;i++){
+			Instantiate(spawnOject, transform.position, Quaternion.identity) ;
+		}
+		}
+		//if(timer%10==0){
+			//Debug.Log(timer*10);
+		//}
+	}
+}
