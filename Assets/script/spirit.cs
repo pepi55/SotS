d1f9f6128f75a082b429398692bae788aa3d10e4
Pepi Dimi <pepi55@live.com>
2013-11-25 15:02:51 +0100

    test

---
+++ Assets/script/spirit.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spirit : WalkingChar {
+	public float time;
+
+	bool timing;
+	bool inTrigger = false;
+
+	float countdown = 0;
+
+	// Use this for initialization
+	void Start () {
+		StartTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timing) {
+			countdown -= Time.deltaTime / 2;
+
+			transform.localScale = new Vector3(countdown, countdown + 2, 1);
+			if (countdown <= 1) {
+				timing = false;
+			}
+		}
+	}
+
+	void FixedUpdate () {
+		ApplyMaxMoveSpeed();
+		ApplySlowdown();
+		print(inTrigger);
+		if (inTrigger) {
+			Walk(countdown);
+		} else {
+			//Walk(-1);
+		}
+		//print("In trigger: " + inTrigger);
+	}
+
+	void StartTimer () {
+		timing = true;
+		countdown = time;
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "walkpath") {
+			print (col.tag);
+			inTrigger = true;
+		} 
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+		if (col.tag == "walkpath") {
+			inTrigger = false;
+		}
+	}
+}
